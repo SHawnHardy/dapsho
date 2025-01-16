c4bc8e52b0f4f161b4d4d812fd9d23d3bb1ce492
Hengyu Shang <shanghengyu997@outlook.com>
2025-01-16 19:48:25 +0800

    feat(util): enhance version handling for Register

--- dapsho/util/register.py
+++ dapsho/util/register.py
@@ -4,9 +4,62 @@
 """
 
 import logging
-from typing import Callable
+from collections.abc import Mapping, Sequence
+from typing import Callable, Optional, override
+
+from semver import Version
 
 logger = logging.getLogger(__name__)
+
+VersionLike = str | Sequence[int | str] | Mapping[str, int | str] | Version
+
+
+def parse_version(version: VersionLike) -> Version:
+    """
+    Parse a semver string/sequence/mapping into a Version object.
+
+    Args:
+        version (Union[str, Sequence[Union[int, str]], Mapping[str, Union[int, str]]]):
+            The version to parse.
+
+    Returns:
+        Version: The parsed version object.
+    """
+    if isinstance(version, Version):
+        return version
+    if isinstance(version, str):
+        version = Version.parse(version, optional_minor_and_patch=True)
+    elif isinstance(version, Sequence):
+        version = Version(*version)
+    elif isinstance(version, Mapping):
+        version = Version(**version)
+    else:
+        raise TypeError("a valid semver must be string | tuple | list | dict")
+    return version
+
+
+def check_version_compatibility(need: VersionLike, have: VersionLike) -> bool:
+    """
+    Check if a version is compatible with another version.
+
+    Args:
+        need (Union[str, Sequence[Union[int, str]], Mapping[str, Union[int, str]]]):
+            The version to check compatibility with.
+        have (Union[str, Sequence[Union[int, str]], Mapping[str, Union[int, str]]]):
+            The version to check compatibility against.
+
+    Returns:
+        bool: True if the versions are compatible, False otherwise.
+    """
+    need = parse_version(need)
+    have = parse_version(have)
+    if need == have:
+        return True
+    if (need.major == 0) or (need.major != have.major):
+        return False
+    if need.prerelease is not None:
+        return False
+    return need < have
 
 
 class Register(dict):
@@ -38,23 +91,93 @@ class Register(dict):
         super().__init__(*args, **kwargs)
         self.name = name
 
-    def register(self, target: Callable | str):
+    def register(self, target: Callable | str, *, version: Optional[str] = None):
 
-        def _add_register_item(key, value):
+        def _add_register_item(key, value, *, version):
             if not isinstance(key, str):
                 raise TypeError("the key for the register must be a string")
             if not callable(value):
                 raise TypeError(f"{repr(value)} is not callable!")
+            if version is None:
+                version = "default"
+            else:
+                try:
+                    version = parse_version(version)
+                except ValueError as e:
+                    raise ValueError(
+                        "the version for the registered callable object must be a valid semver"
+                    ) from e
 
-            if key in self:
+            # pylint: disable=super-with-arguments
+            if key in self and version in super(Register, self).__getitem__(key):
                 logger.warning("register %s: '%s' will be overridden!", self.name, key)
-            self[key] = value
-            logger.debug("register %s: '%s' has been registered", self.name, key)
+            self.setdefault(key, {})[version] = value
+            logger.debug(
+                "register %s: '%s' ver.%s has been registered", self.name, key, version
+            )
             return value
 
         if callable(target):
-            return _add_register_item(target.__name__, target)
-        return lambda x: _add_register_item(target, x)
+            return _add_register_item(target.__name__, target, version=version)
+        return lambda x: _add_register_item(target, x, version=version)
 
-    def __call__(self, target: Callable | str):
-        return self.register(target)
+    @override
+    def __getitem__(self, key):
+        if isinstance(key, str):
+            version = "default"
+        else:
+            key, version = key[0], key[1]
+        if key not in self:
+            raise KeyError(f"register {self.name}: '{key}' not found")
+        return super().__getitem__(key)[version]
+
+    def get_latest(
+        self, key: str, *, compatible_with: Optional[VersionLike] = None
+    ) -> Callable:
+        """
+        Get the latest registered callable for a given key,
+            optionally compatible with a specific version.
+
+        Args:
+            key (str): The key for the registered callable.
+            compatible_with (Optional[VersionLike]): The version to check compatibility with.
+
+        Returns:
+            Callable: The latest registered callable that is compatible with the specified version.
+
+        Raises:
+            KeyError: If the key is not found in the register.
+        """
+        if key not in self:
+            raise KeyError(f"register {self.name}: '{key}' not found")
+
+        versions = super().__getitem__(key).keys()
+
+        if compatible_with is not None:
+            compatible_with = parse_version(compatible_with)
+            versions = filter(
+                lambda x: isinstance(x, Version)
+                and check_version_compatibility(compatible_with, x),
+                versions,
+            )
+            try:
+                return super().__getitem__(key)[max(versions)]
+            except ValueError as e:
+                raise KeyError(
+                    f"register {self.name}: '{key}' ver.{compatible_with} has no compatible version"
+                ) from e
+
+        if len(versions) == 1:
+            return next(iter(super().__getitem__(key).values()))
+
+        try:
+            return super().__getitem__(key)[
+                max(filter(lambda x: isinstance(x, Version), versions))
+            ]
+        except ValueError as e:
+            raise KeyError(
+                f"register {self.name}: '{key}' has no registered version"
+            ) from e
+
+    def __call__(self, target: Callable | str, *, version: Optional[str] = None):
+        return self.register(target, version=version)
--- test/util/test_register.py
+++ test/util/test_register.py
@@ -6,8 +6,32 @@
 import logging
 
 import pytest
+from semver import Version
 
-from dapsho.util.register import Register
+from dapsho.util.register import Register, check_version_compatibility, parse_version
+
+
+def test_parse_version():
+    assert parse_version("1.0.0") == Version(1, 0, 0)
+    assert parse_version([1, 0, 0]) == Version(1, 0, 0)
+    assert parse_version({"major": 1, "minor": 0, "patch": 0}) == Version(1, 0, 0)
+    assert parse_version(Version(1, 0, 0)) == Version(1, 0, 0)
+    with pytest.raises(TypeError):
+        parse_version(123)
+
+
+def test_check_version_compatibility():
+    assert check_version_compatibility("1.0.0", "1.0.0")
+    assert check_version_compatibility("1.0.0", "1.1.0")
+    assert not check_version_compatibility("1.0.0", "2.0.0")
+    assert not check_version_compatibility("1.0.0-alpha", "1.0.0")
+    assert check_version_compatibility("1.0.0", "1.0.1")
+    assert not check_version_compatibility("0.1.0", "0.2.0")
+
+
+def test_parse_version_invalid_type():
+    with pytest.raises(TypeError):
+        parse_version(123)
 
 
 def test_register():
@@ -24,17 +48,34 @@ def test_register():
     assert register_test["tfoo"]() == "foo"
     assert register_test["tbar"]() == "bar"
 
-    @register_test("tbar")
+    @register_test("tbar", version="1.0.0")
     def tbar2():
-        return "bar_overwritten"
+        return "bar_v2"
 
-    assert register_test["tbar"]() == "bar_overwritten"
+    assert register_test[("tbar", Version(1, 0, 0))]() == "bar_v2"
+
+    with pytest.raises(KeyError):
+        id(register_test["baz"])
 
 
 def test_register_non_string_key():
     register_test = Register("test")
     with pytest.raises(TypeError):
         register_test.register(123)(lambda: "foo")
+
+
+def test_register_override():
+    register_test = Register("test")
+
+    @register_test
+    def tfoo():
+        return "foo"
+
+    @register_test("tfoo")
+    def tfoo2():
+        return "foo_override"
+
+    assert register_test["tfoo"]() == "foo_override"
 
 
 def test_register_override_warning(caplog):
@@ -51,3 +92,34 @@ def test_register_override_warning(caplog):
             return "foo_overwritten"
 
     assert "will be overridden" in caplog.text
+
+
+def test_get_latest():
+    register_test = Register("test")
+
+    @register_test("foo", version="1.0.0")
+    def foo_v1():
+        return "foo_v1"
+
+    @register_test("foo", version="1.1.0")
+    def foo_v1_1():
+        return "foo_v1_1"
+
+    @register_test("foo", version="2.0.1")
+    def foo_v2():
+        return "foo_v2"
+
+    assert register_test.get_latest("foo")() == "foo_v2"
+    assert register_test.get_latest("foo", compatible_with="1.0.0")() == "foo_v1_1"
+    assert register_test.get_latest("foo", compatible_with="1.1.0")() == "foo_v1_1"
+    with pytest.raises(KeyError):
+        register_test.get_latest("foo", compatible_with="1.2.0")()
+    with pytest.raises(KeyError):
+        register_test.get_latest("foo", compatible_with="3.0.0")()
+    with pytest.raises(KeyError):
+        register_test.get_latest("foo", compatible_with="2.1.0")()
+    assert register_test.get_latest("foo", compatible_with="2.0.0")() == "foo_v2"
+    assert register_test.get_latest("foo", compatible_with="2.0.0")() == "foo_v2"
+
+    with pytest.raises(KeyError):
+        register_test.get_latest("bar")
